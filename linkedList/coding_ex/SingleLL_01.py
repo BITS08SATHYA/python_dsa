0f7fd0cbd088517309b36a33f13531538a38fa3a
Sathya <neelasathya06@gmail.com>
2025-03-04 22:27:37 -0500

    Linked List prepend operation implemented

--- linkedList/coding_ex/SingleLL_01.py
+++ linkedList/coding_ex/SingleLL_01.py
@@ -10,3 +10,13 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
