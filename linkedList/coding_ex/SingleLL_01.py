dc346d2504c9fd39d32498161fb69b76d1a2db2b
Sathya <neelasathya06@gmail.com>
2025-03-15 23:49:37 -0400

    Reversed a single LinkedList

--- linkedList/coding_ex/SingleLL_01.py
+++ linkedList/coding_ex/SingleLL_01.py
@@ -58,3 +58,13 @@ class LinkedList:
             popped_node.next = None
             self.length -= 1
             return popped_node
+
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head, self.tail = self.tail, self.head
