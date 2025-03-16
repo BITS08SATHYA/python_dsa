0e084d8fe87db751c32a0f7ac41c2faacf26b34d
Sathya <neelasathya06@gmail.com>
2025-03-15 23:55:12 -0400

    Remove duplicates from a single linked list

--- linkedList/coding_ex/SingleLL_01.py
+++ linkedList/coding_ex/SingleLL_01.py
@@ -68,3 +68,19 @@ class LinkedList:
             prev_node = current_node
             current_node = next_node
         self.head, self.tail = self.tail, self.head
+
+    def remove_duplicates(self):
+        if self.head is None:
+            return None
+        node_values = set()  # set to store unique values
+        current_node = self.head
+        node_values.add(current_node.value)
+        while current_node.next:
+            if current_node.next.value in node_values:
+                current_node.next = current_node.next.next
+                self.length -= 1
+            else:
+                node_values.add(current_node.next.value)
+                current_node =  current_node.next
+        self.tail = current_node
+
