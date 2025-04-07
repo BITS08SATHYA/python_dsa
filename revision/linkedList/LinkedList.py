ed30ba013a61cce052823d891e260d351b27a8bf
Sathya <neelasathya06@gmail.com>
2025-04-07 16:21:55 -0400

    129-142 --> linked list revision

---
+++ revision/linkedList/LinkedList.py
@@ -0,0 +1,87 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# class LinkedList:
+#     def __init__(self,value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def insert(self, index, value):
+        new_node = Node(value)
+        if index < 0 or index > self.length:
+            return False
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+            for _ in range(index-1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length += 1
+        return True
+
+    def traverse(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.value)
+            curr = curr.next
+
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
+
+
+
+
+new_linked_list = LinkedList()
+new_linked_list.append(1)
+new_linked_list.append(2)
+new_linked_list.prepend(3)
+new_linked_list.insert(2, 5)
+# print(new_linked_list.__str__())
+new_linked_list.traverse()
+# print(new_linked_list.head.value)
+# print(new_linked_list.head.next.value)
