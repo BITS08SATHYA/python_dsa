ea2f5d4150e6b7db100cce1df2db97f516d9ab37
Sathya <neelasathya06@gmail.com>
2025-04-13 21:28:22 -0400

    169. Prepend method done

--- revision/DSA_LL/CircularSingleLinkedList/CSLinkedList.py
+++ revision/DSA_LL/CircularSingleLinkedList/CSLinkedList.py
@@ -40,10 +40,24 @@ class CSLinkedList:
             result += '->'
         return result
 
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.tail.next = new_node
+        self.length += 1
+
+
 
 
 
 csll = CSLinkedList()
+csll.prepend(50)
 csll.append(1)
 csll.append(2)
 
