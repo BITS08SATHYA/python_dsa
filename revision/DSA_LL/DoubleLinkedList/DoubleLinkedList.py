67ddc9de52d7751bae0cce0f7acfc163ff223084
Sathya <neelasathya06@gmail.com>
2025-04-16 11:19:12 -0400

    189. Append Method in DLL

---
+++ revision/DSA_LL/DoubleLinkedList/DoubleLinkedList.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+    def __str__(self):
+        return str(self.value)
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
+
+
+newDLL = DoubleLinkedList()
+newDLL.append(1)
+newDLL.append(2)
+print(newDLL.head.value)
+print(newDLL.head.next.value)
