94446a6539692f2e2dbe8153997e2d1b924c3b3b
Sathya <neelasathya06@gmail.com>
2025-04-17 20:13:11 -0400

    203. CDLL append method implemented

--- revision/DSA_LL/CircularDoubleLL/CircularDoubleLL.py
+++ revision/DSA_LL/CircularDoubleLL/CircularDoubleLL.py
@@ -21,6 +21,21 @@ class CircularDoubleLL:
         self.tail = None
         self.length = 0
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            new_node.prev = new_node
+            new_node.next = new_node
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.head.prev = new_node
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1
+
 
 
 new_cdll = CircularDoubleLL(10)
