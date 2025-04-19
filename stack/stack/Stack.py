bcebfb83501d6e8374a3f3e930fee84164852902
Sathya <neelasathya06@gmail.com>
2025-04-18 22:37:49 -0400

    230. Stack str method implemented

--- stack/stack/Stack.py
+++ stack/stack/Stack.py
@@ -1,3 +1,13 @@
 class Stack:
     def __init__(self):
-        pass
+        self.list = []
+
+    # def __str__(self):
+    #     values = self.list.reverse()
+    #     values = [str(x) for x in self.list]
+    #     return '\n'.join(values)
+
+    def __str__(self):
+        values = [str(x) for x in reversed(self.list)]
+        return '\n'.join(values)
+
