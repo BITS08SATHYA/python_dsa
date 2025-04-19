648b5364f1b6637d30bbd91646a72aaae4dfa425
Sathya <neelasathya06@gmail.com>
2025-04-19 11:48:37 -0400

    245. Queue LL implemented

---
+++ queue_03/Queue_LL.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        curNode = self.head
+        while curNode:
+            yield curNode
+            curNode = curNode.next
+
+class Queue:
+    def __init__(self):
+        self.LinkedList = LinkedList()
+
+    def __str__(self):
+        values = [str(x) for x in self.LinkedList]
+        return ' '.join(values)
+
+    def enqueue(self, value):
+        newNode = Node(value)
+        if self.LinkedList.head is None:
+            self.LinkedList.head = newNode
+            self.LinkedList.tail = newNode
+        else:
+            self.LinkedList.tail.next = newNode
+            self.LinkedList.tail = newNode
+
+
+custQueue = Queue()
+custQueue.enqueue(1)
+custQueue.enqueue(2)
+custQueue.enqueue(3)
+print(custQueue)
