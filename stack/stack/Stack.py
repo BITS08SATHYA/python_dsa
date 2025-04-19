00540264591270be17abadd76f6f8f4e1bb59aa9
Sathya <neelasathya06@gmail.com>
2025-04-18 22:43:33 -0400

    230. Stack push, pop, peek, delete method implemented

--- stack/stack/Stack.py
+++ stack/stack/Stack.py
@@ -11,3 +11,38 @@ class Stack:
         values = [str(x) for x in reversed(self.list)]
         return '\n'.join(values)
 
+    def isEmpty(self):
+        if not self.list:
+            return True
+        else:
+            return False
+
+    def push(self,value):
+        self.list.append(value)
+        return "The element has been successfully inserted at the end!"
+
+    def pop(self):
+        if self.isEmpty():
+            return "There is not any elt in the stack!"
+        else:
+            return self.list.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return "There is not any element in teh stack"
+        else:
+            return self.list[len(self.list) - 1]
+
+    def delete(self):
+        self.list = None
+
+customStack = Stack()
+print(customStack.isEmpty())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+print(customStack.__str__())
+customStack.pop()
+
+print(customStack.__str__())
+
