775d527da9fac633dc05020aeb2b1ebcd22f7466
Sathya <neelasathya06@gmail.com>
2025-04-26 15:01:19 -0400

    Binary tree method pre-order and in-order traversal implemented successfully

--- tree_06/BinaryTree/TreeNode.py
+++ tree_06/BinaryTree/TreeNode.py
@@ -5,4 +5,31 @@ class TreeNode:
         self.rightChild = None
 
 newBT = TreeNode("Drinks")
-print(newBT.data)
+# print(newBT.data)
+leftChild = TreeNode("Hot")
+rightChild = TreeNode("Cold")
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+print('PreOrder Traversal')
+print('-----------------')
+preOrderTraversal(newBT)
+print('\n')
+print('InOrder')
+print('-----------------')
+inOrderTraversal(newBT)
