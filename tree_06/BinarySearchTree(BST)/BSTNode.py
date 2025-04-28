4aafab9abd8bd49976f86b20497fcb9e1881f0db
Sathya <neelasathya06@gmail.com>
2025-04-28 15:42:31 -0400

    Binary search Tree - insertion implemented

--- tree_06/BinarySearchTree(BST)/BSTNode.py
+++ tree_06/BinarySearchTree(BST)/BSTNode.py
@@ -5,4 +5,22 @@ class BSTNode:
         self.rightChild = None
 
 
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.rightChild, nodeValue)
+    return "The node has been inserted"
+
+
 newBST = BSTNode(None)
+print(insertNode(newBST, 1))
+print(insertNode(newBST, 2))
