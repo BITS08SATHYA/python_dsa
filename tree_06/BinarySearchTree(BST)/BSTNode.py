c9ba65dfc0fa44672d5898153adfd1ad7e0af386
Sathya <neelasathya06@gmail.com>
2025-04-28 15:58:28 -0400

    Binary search Tree - traversal (pre,in, post and level order) implemented

--- tree_06/BinarySearchTree(BST)/BSTNode.py
+++ tree_06/BinarySearchTree(BST)/BSTNode.py
@@ -1,3 +1,5 @@
+from queue_03.queue.Queue_LL import Queue as Queue
+
 class BSTNode:
     def __init__(self, data):
         self.data = data
@@ -20,7 +22,49 @@ def insertNode(rootNode, nodeValue):
             insertNode(rootNode.rightChild, nodeValue)
     return "The node has been inserted"
 
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            print(root.value.data)
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+
 
 newBST = BSTNode(None)
-print(insertNode(newBST, 1))
-print(insertNode(newBST, 2))
+insertNode(newBST, 40)
+insertNode(newBST, 8)
+insertNode(newBST, 1)
+insertNode(newBST, 2)
+insertNode(newBST, 4)
+
+
+# preOrderTraversal(newBST)
+levelOrderTraversal(newBST)
