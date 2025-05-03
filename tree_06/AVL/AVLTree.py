2a90d533465e10effac1a89cd935cf7e1f58a892
Sathya <neelasathya06@gmail.com>
2025-05-03 00:18:28 -0400

    AVL - Insertion implemented

--- tree_06/AVL/AVLTree.py
+++ tree_06/AVL/AVLTree.py
@@ -61,8 +61,52 @@ def getHeight(rootNode):
         return 0
     return rootNode.height
 
-newAVL = AVLNode(10)
+def rightRotate(disbalanceNode):
+    newRoot = disbalanceNode.leftChild
+    disbalanceNode.leftChild = disbalanceNode.leftChild.rightChild
+    newRoot.rightChild = disbalanceNode
+    disbalanceNode.height = 1 + max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
+    return newRoot
 
-# this is the cooment
+def leftRotate(disbalanceNode):
+    newRoot = disbalanceNode.rightChild
+    disbalanceNode.rightChild = disbalanceNode.rightChild.leftChild
+    newRoot.leftChild = disbalanceNode
+    disbalanceNode.height = 1 + max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
+    return newRoot
 
-preOrderTraversal(newAVL)
+def getBalance(rootNode):
+    if not rootNode:
+        return 0
+    return getHeight(rootNode.leftChild) - getHeight(rootNode.rightChild)
+
+def insertNode(rootNode, nodeValue):
+    if not rootNode:
+        return AVLNode(nodeValue)
+    elif nodeValue < rootNode.data:
+        rootNode.leftChild = insertNode(rootNode.leftChild, nodeValue)
+    else:
+        rootNode.rightChild = insertNode(rootNode.rightChild, nodeValue)
+    rootNode.height = 1 + max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild))
+    balance = getBalance(rootNode)
+    if balance > 1 and nodeValue < rootNode.leftChild.data:
+        return rightRotate(rootNode)
+    if balance > 1 and nodeValue > rootNode.rightChild.data:
+        return rightRotate(rootNode)
+    if balance < -1 and nodeValue > rootNode.rightChild.data:
+        return leftRotate(rootNode)
+    if balance < -1 and nodeValue < rootNode.rightChild.data:
+        rootNode.rightChild = rightRotate(rootNode.rightChild)
+        return leftRotate(rootNode)
+    return rootNode
+
+newAVL = AVLNode(5)
+
+# this is the comment
+newAVL = insertNode(newAVL, 10)
+newAVL = insertNode(newAVL, 15)
+newAVL = insertNode(newAVL, 20)
+levelOrderTraversal(newAVL)
+# preOrderTraversal(newAVL)
