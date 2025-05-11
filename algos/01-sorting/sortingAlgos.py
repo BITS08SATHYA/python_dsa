fea39b8397f3b5c0089c907435b552c5e71e4dce
Sathya <neelasathya06@gmail.com>
2025-05-11 11:42:00 +0530

    insertion sort implemented

--- algos/01-sorting/sortingAlgos.py
+++ algos/01-sorting/sortingAlgos.py
@@ -2,6 +2,7 @@ cList = [2,1,4,5,7,8,4,3,1]
 
 
 # Time Complexity - O(n^2)
+# Takes two adjacent elements and swap them in multiple for loops until the list has been sorted
 def bubbleSort(customList):
     for i in range(len(customList) - 1):
         for j in range(len(customList) - i - 1):
@@ -12,6 +13,7 @@ def bubbleSort(customList):
 # bubbleSort(customList)
 
 # Time Complexity - O(n^2)
+# find the minimum index and swap that respective element with the current element until the list has been sorted
 def selectionSort(customList):
     for i in range(len(customList)):
         min_index = i
@@ -21,5 +23,18 @@ def selectionSort(customList):
         customList[i], customList[min_index] = customList[min_index], customList[i]
     print(customList)
 
-selectionSort(cList)
+# selectionSort(cList)
+
+# Insertion sort
+def insertionSort(customList):
+    for i in range(1, len(customList)):
+        key = customList[i]
+        j = i - 1
+        while j >= 0 and key < customList[j]:
+            customList[j + 1] = customList[j]
+            j -= 1
+        customList[j + 1] = key
+    print(customList)
+
+insertionSort(cList)
 
