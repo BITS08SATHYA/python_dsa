c4e0edbe7b0365ee7db2bb47d8e7c3de65f4ca5c
Sathya <neelasathya06@gmail.com>
2025-05-11 11:17:47 +0530

    selection sort implemented

--- algos/01-sorting/sortingAlgos.py
+++ algos/01-sorting/sortingAlgos.py
@@ -1,3 +1,7 @@
+cList = [2,1,4,5,7,8,4,3,1]
+
+
+
 def bubbleSort(customList):
     for i in range(len(customList) - 1):
         for j in range(len(customList) - i - 1):
@@ -5,6 +9,16 @@ def bubbleSort(customList):
                 customList[j], customList[j + 1] = customList[j + 1], customList[j]
     print(customList)
 
-cList = [2,1,4,5,7,8,4,3,1]
-bubbleSort(cList)
+# bubbleSort(customList)
+
+def selectionSort(customList):
+    for i in range(len(customList)):
+        min_index = i
+        for j in range(i+1, len(customList)):
+            if customList[min_index] > customList[j]:
+                min_index = j
+        customList[i], customList[min_index] = customList[min_index], customList[i]
+    print(customList)
+
+selectionSort(cList)
 
