a7fc61b2d6670e2a445748a1d3e891eca1af265d
Sathya <neelasathya06@gmail.com>
2025-05-12 07:28:07 +0530

    linear search implemented

---
+++ algos/02-searching/linear_search.py
@@ -0,0 +1,16 @@
+def linearSearch(array, value):
+    for i in range(len(array)):
+        if array[i] == value:
+            return i
+    return -1
+
+
+
+# print(linearSearch([20,40,30,50, 90], 50))
+
+
+def binarySearch(array, value):
+    pass
+
+
+
