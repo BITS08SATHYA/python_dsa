38a8bd4bc8c3e4402bd5905db88ac120f3cd45b8
Sathya <neelasathya06@gmail.com>
2025-05-12 07:38:15 +0530

    binary search implemented

--- algos/02-searching/linear_search.py
+++ algos/02-searching/linear_search.py
@@ -1,3 +1,4 @@
+import math
 def linearSearch(array, value):
     for i in range(len(array)):
         if array[i] == value:
@@ -10,7 +11,21 @@ def linearSearch(array, value):
 
 
 def binarySearch(array, value):
-    pass
+    start = 0
+    end = len(array) - 1
+    middle = math.floor((start + end) / 2)
+    while not(array[middle] == value) and start <= end:
+        if value < array[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+        middle = math.floor((start + end) / 2)
+    if array[middle] == value:
+        return middle
+    else:
+        return -1
 
+custArray = [8,9,12,15,17,19,20,21,28]
+print(binarySearch(custArray, 15))
 
 
