5e6620ae56c955d0fad21d5fced5ff83fb99f951
Sathya <neelasathya06@gmail.com>
2025-05-13 06:47:44 +0530

    graph data structure using python with breadth first traversal implemented

--- algos/03-graphs/GraphUsingPython.py
+++ algos/03-graphs/GraphUsingPython.py
@@ -33,6 +33,30 @@ class Graph:
             return True
         return False
 
+    def bfs(self, vertex):
+        visited = set()
+        visited.add(vertex)
+        queue = [vertex]
+        while queue:
+            current_vertex = queue.pop(0)
+            print(current_vertex)
+            for adjacent_vertex in self.adjacency_list[current_vertex]:
+                if adjacent_vertex not in visited:
+                    visited.add(adjacent_vertex)
+                    queue.append(adjacent_vertex)
+
+    def dfs(self, vertex):
+        visited = set()
+        stack = [vertex]
+        while stack:
+            current_vertex = stack.pop()
+            if current_vertex not in visited:
+                print(current_vertex)
+                visited.add(current_vertex)
+                for adjacent_vertex in self.adjacency_list[current_vertex]:
+                    pass
+
+
     def print_graph(self):
         for vertex in self.adjacency_list.keys():
             print(vertex, ":", self.adjacency_list[vertex])
@@ -43,13 +67,15 @@ custom_graph.add_vertex("A")
 custom_graph.add_vertex("B")
 custom_graph.add_vertex("C")
 custom_graph.add_vertex("D")
+custom_graph.add_vertex("E")
 custom_graph.add_Edge("A", "B")
 custom_graph.add_Edge("A", "C")
-custom_graph.add_Edge("A", "D")
-custom_graph.add_Edge("B", "C")
+custom_graph.add_Edge("B", "E")
 custom_graph.add_Edge("C", "D")
+custom_graph.add_Edge("D", "E")
 custom_graph.print_graph()
 # custom_graph.remove_edge("A", "C")
-custom_graph.remove_vertex("D")
-print("Print after remove edge")
-custom_graph.print_graph()
+# custom_graph.remove_vertex("D")
+# print("Print after remove edge")
+# custom_graph.print_graph()
+print(custom_graph.bfs("A"))
